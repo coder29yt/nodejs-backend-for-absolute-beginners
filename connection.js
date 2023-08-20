79af87643beee41accf5ce7be4b352eaf0630370
coder29yt <134164127+coder29yt@users.noreply.github.com>
2023-08-20 22:07:58 +0530

    Update connection.js with a note

--- connection.js
+++ connection.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 
+// PLEASE ENTER YOUR OWN CONNECTION URI HERE, DONT USE MINE 
 const MONGO_URI ="mongodb+srv://rajpadval145:rajpadval145@cluster0.hbvhhwg.mongodb.net/todolist?retryWrites=true&w=majority";
 
 const connectDB = async () => {
